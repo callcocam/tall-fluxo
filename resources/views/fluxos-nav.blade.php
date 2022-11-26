92de52a6d7d14fe9d373cf3a1a94992898cff7b2
bengsinc <111383298+bengsinc@users.noreply.github.com>
2022-11-26 14:56:25 -0300

    Ajustes Nav

--- resources/views/fluxos-nav.blade.php
+++ resources/views/fluxos-nav.blade.php
@@ -44,7 +44,7 @@
 
                 @endforeach
                 <div class="space-y-1">
-                    <a href=""
+                    <a href="{{route('admin.produtos')}}"
                        class="group flex items-center px-2 py-2 text-sm font-medium rounded-md text-gray-700 hover:text-gray-900 hover:bg-gray-50">
                         <x-dynamic-component component="tall::icons.outline.plus"
                                              class="mr-1 h-4 w-4 flex-shrink-0 " />
