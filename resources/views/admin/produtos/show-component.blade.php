465e18f30ec8bb760d98841e93bb0c306f624daf
bengsinc <111383298+bengsinc@users.noreply.github.com>
2022-11-26 17:44:39 -0300

    ..

--- resources/views/admin/produtos/show-component.blade.php
+++ resources/views/admin/produtos/show-component.blade.php
@@ -7,11 +7,9 @@
         <div class="min-w-screen min-h-screen bg-gray-100 flex  font-sans overflow-hidden">
             <div class="w-full">
                 <div class="bg-white shadow-md rounded">
-                    <!-- This example requires Tailwind CSS v2.0+ -->
                     <div class="bg-white shadow overflow-hidden sm:rounded-lg">
                         <div class="px-4 py-5 sm:px-6">
                             <h3 class="text-lg leading-6 font-medium text-gray-900">{{ $model->nome_produto }}</h3>
-                            {{-- <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p> --}}
                         </div>
                         <div class="border-t border-gray-200">
                             @if ($fluxo_etapa_items = $this->fluxo_etapa_items)
